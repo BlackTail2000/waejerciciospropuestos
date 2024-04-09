8ef985a788220e106ca328e7d110b8198f683708
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-04-09 11:24:46 -0500

    Ejercicio 2 Completado

--- src/main/java/pe/edu/cibertec/waejerciciospropuestos/controller/EjerciciosController.java
+++ src/main/java/pe/edu/cibertec/waejerciciospropuestos/controller/EjerciciosController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/Ejercicios")
 public class EjerciciosController {
+
     @GetMapping("/Uno")
     public String ejercicioUno(){
         return "primerejercicio";
@@ -23,5 +24,41 @@ public class EjerciciosController {
         model.addAttribute("areatotal", 2 * 3.14 * (r + h));
         System.out.print("Estas aquí");
         return "primerejercicio";
+    }
+
+    @GetMapping("/Dos")
+    public String ejercicioDos() {
+        return "segundoejercicio";
+    }
+
+    @PostMapping("/Dos")
+    public String ejercicioDos(@RequestParam("m") int m,
+                               @RequestParam("a") int a,
+                               Model model){
+        String nombreMes = switch (m) {
+            case 1 -> "Enero";
+            case 2 -> "Febrero";
+            case 3 -> "Marzo";
+            case 4 -> "Abril";
+            case 5 -> "Mayo";
+            case 6 -> "Junio";
+            case 7 -> "Julio";
+            case 8 -> "Agosto";
+            case 9 -> "Septiembre";
+            case 10 -> "Octubre";
+            case 11 -> "Noviembre";
+            default -> "Diciembre";
+        };
+
+        int cantidadDias = switch (m) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
+            default -> a % 4 == 0 && (a % 100 != 0 || a % 400 == 0) ? 29 : 28;
+        };
+
+        model.addAttribute("nombreMes", nombreMes);
+        model.addAttribute("cantidadDias", cantidadDias);
+        model.addAttribute("anio", a);
+        return "segundoejercicio";
     }
 }
